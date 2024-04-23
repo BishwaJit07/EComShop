ce61626a04136b82c2539d22058fd232109c70ad
Saiful264 <saifulislamx264@gmail.com>
2024-04-23 12:57:30 +0600

    Make my own branch

---
+++ src/app/test/page.js
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const test = () => {
+    return (
+        <div>
+            <Button>this is a Sadcdn Button</Button>
+        </div>
+    );
+};
+
+export default test;
